48a145a5fa0c6275058cf8da6ea3dd1dd56db4fa
chandu-313 <chanduneeraj313@gmail.com>
2024-11-30 17:21:34 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("my name is chandu")
+print("i have",5,"pens and",5,"pencils")
+print("we were having two plus three",2 + 3,"pets")
+#python is an interpreter language made by guido van rossum
